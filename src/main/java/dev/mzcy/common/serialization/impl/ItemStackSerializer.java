0b67417d5b64c2d1d8df166bb9e2f38e5351f10a
KeineSecrets <miethe.max@web.de>
2025-05-27 19:06:58 +0200

    Refactor ItemStack serialization to handle null and air cases, and update NBT handling

--- src/main/java/dev/mzcy/common/serialization/impl/ItemStackSerializer.java
+++ src/main/java/dev/mzcy/common/serialization/impl/ItemStackSerializer.java
@@ -3,9 +3,11 @@ package dev.mzcy.common.serialization.impl;
 import dev.mzcy.common.serialization.Serializer;
 import dev.mzcy.common.serialization.exception.DeserializeException;
 import dev.mzcy.common.serialization.exception.SerializeException;
+import net.kyori.adventure.nbt.CompoundBinaryTag;
+import net.kyori.adventure.nbt.TagStringIOExt;
+import net.kyori.examination.string.StringExaminer;
+import net.minestom.server.adventure.serializer.nbt.NbtComponentSerializer;
 import net.minestom.server.item.ItemStack;
-import org.jglrxavpok.hephaistos.nbt.NBTCompound;
-import org.jglrxavpok.hephaistos.nbt.NBTReader;
 
 import java.util.Base64;
 
@@ -18,15 +20,12 @@ public class ItemStackSerializer implements Serializer<ItemStack, String> {
      */
     @Override
     public String serialize(ItemStack object) throws SerializeException {
-        if (object == null) {
-            return null;
-        }
         try {
-            NBTCompound nbt = object.toItemNBT();
-            byte[] nbtBytes = nbt.toByteArray();
-            return Base64.getEncoder().encodeToString(nbtBytes);
+            if (object == null || object.isAir()) return "";
+            CompoundBinaryTag nbt = object.toItemNBT();
+            return Base64.getEncoder().encodeToString(TagStringIOExt.writeTag(nbt).getBytes());
         } catch (Exception e) {
-            throw new SerializeException("Failed to serialize ItemStack: " + e.getMessage(), e);
+            throw new SerializeException("Failed to serialize ItemStack", e);
         }
     }
 
@@ -38,14 +37,13 @@ public class ItemStackSerializer implements Serializer<ItemStack, String> {
      */
     @Override
     public ItemStack deserialize(String object) throws DeserializeException {
-        if (object == null || object.isEmpty()) {
-            return null;
-        }
-        byte[] nbtBytes = Base64.getDecoder().decode(object);
-        try (NBTReader nbtReader = NBTReader.fromArray(nbtBytes)) {
-            return ItemStack.fromItemNBT(((NBTCompound) nbtReader.read()));
+        try {
+            if (object == null || object.isEmpty()) return ItemStack.AIR;
+            byte[] bytes = Base64.getDecoder().decode(object);
+            CompoundBinaryTag nbt = CompoundBinaryTag.empty().put("", TagStringIOExt.readTag(new String(bytes)));
+            return ItemStack.fromItemNBT(nbt);
         } catch (Exception e) {
-            throw new DeserializeException("Failed to deserialize ItemStack: " + e.getMessage(), e);
+            throw new DeserializeException("Failed to deserialize ItemStack", e);
         }
     }
 }
